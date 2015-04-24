69ab79648e5e9190b302500a8bdec6e8dc3ffc6d
Thomas Bloss <runredfox@gmail.com>
2015-04-24 15:35:59 -0400

    coded

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -13,14 +13,39 @@
 @end
 
 @implementation AppDelegate
-            
+
+int multiplies = 8;
+int divisor = 6;
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    // Insert code here...
     
+    
+    [self iterateCount:4];
     
     return YES;
 }
-
+-(void)iterateCount:(int)decreasingNumber {
+    if (decreasingNumber > 0){
+        NSLog(@"Iterate %d", decreasingNumber);
+        [self iterateCount:--decreasingNumber];
+        int newMultipliedNumber=[self multiplied:decreasingNumber];
+        
+        NSLog(@"Multiplied %d by %d to be %d", decreasingNumber, multiplies, newMultipliedNumber);
+        [self divided:newMultipliedNumber];
+        float dividedNumber = [self divided:newMultipliedNumber];
+        NSLog(@"Divided %d by %d to be %f", newMultipliedNumber, divisor, dividedNumber);
+        
+    } else {
+        return;
+    }
+    
+}
+-(int)multiplied:(int)multipliedNumber {
+    return multiplies * multipliedNumber;
+    
+}
+-(float)divided:(float)dividedNumber {
+    return dividedNumber / divisor;
+}
 @end
